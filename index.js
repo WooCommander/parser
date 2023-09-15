b0d05df22454fd6b3c17df5c8219a163fe54edf7
ocnelavoc <ocnelavoc@gmail.com>
2023-09-15 16:14:31 +0300

    upd routes

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import express from "express";
-import path from "path";
-import serverRoters from "./routers/routers";
+import serverRoters from "./routers/routers.js";
+
 const app = express();
-const __dirname = path.resolve();
-let PORT = process.env.PORT ?? 3001;
+
+let PORT = process.env.PORT ?? 3000;
 app.use(serverRoters);
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "static", "index.html"));
-});
+
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}...`);
 });
--- routers/routers.js
+++ routers/routers.js
@@ -1,6 +1,11 @@
 import { Router } from "express";
-import { getAll } from "../controllers/servers";
+import { getAll } from "../controllers/servers.js";
+const __dirname = path.resolve();
+import path from "path";
 const router = Router();
 
 router.get("/api/server", getAll);
+router.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "static", "index.html"));
+});
 export default router;
